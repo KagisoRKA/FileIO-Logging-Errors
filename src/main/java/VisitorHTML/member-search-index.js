705c4b5a2dc853f9e1191aa89dfddef71563a507
Kagiso Raseroka <kagiso.raseroka@umuzi.org>
2020-01-16 15:09:26 +0200

    Making updates to project

--- src/main/java/VisitorHTML/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"Visitor","l":"load(String)","url":"load(java.lang.String)"},{"p":"<Unnamed>","c":"Visitor","l":"save()"},{"p":"<Unnamed>","c":"Visitor","l":"Visitor(String, int, String, String, String, String)","url":"%3Cinit%3E(java.lang.String,int,java.lang.String,java.lang.String,java.lang.String,java.lang.String)"}]
